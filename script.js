de7f6ca577d910cb898e0329e9847926cca6a946
ivo172 <ivo172@gmail.com>
2021-01-17 19:10:41 +0100

    ok

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-let js = 'amazing';
+// let js = 'amazing';
+// 
+// if (js === 'amazing') alert('JavaScript is FUN!');
+// 
+//  
+// let PI = 3.1415;
+// 
+// console.log (PI);
+// 
+// console.log(typeof PI);
 
-if (js === 'amazing') alert('JavaScript is FUN!');
+const now  = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now -2018
 
- 
-let PI = 3.1415;
-
-console.log (PI);
-
-console.log(typeof PI);
-
+console.log(now-1991 > now - 2018)
