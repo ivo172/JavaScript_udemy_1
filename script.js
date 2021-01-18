f684bb0fff96c3bc53618fc4c1cb312bca610e89
ivo172 <ivo172@gmail.com>
2021-01-18 19:17:07 +0100

    okt

--- script.js
+++ script.js
@@ -9,8 +9,41 @@
 // 
 // console.log(typeof PI);
 
-const now  = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now -2018
+// const now  = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now -2018
+// 
+// console.log(now-1991 > now - 2018)
 
-console.log(now-1991 > now - 2018)
+
+// const age = 15;
+// 
+// if (age >= 18) {
+//     console.log('You can start driving license !')
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`)    
+// }
+
+
+// type conversion
+
+
+const inputYear = '1991';
+
+console.log(Number(inputYear) + 18);
+
+console.log(String(23), 23);
+
+
+// type coercion
+
+console.log('I am ' + 23 + ' years old.')
+console.log('23' - '10' - 3);
+console.log('23' / '2');
+console.log('23' > '18');
+
+let n = '1' + 1; // '11'
+n = n - 1;
+console.log(n);
+
